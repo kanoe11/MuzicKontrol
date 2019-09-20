0b2e00a9f3735121273663ce82a47ef31193f54b
kanoe <noe.ngongo20@gmail.com>
2019-09-20 19:17:45 +0200

    add php + articles

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Muzikkontrol</title>
+  <title>Muzikcontrol</title>
   <link href="libraries/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
   <link href="css/style.css" rel="stylesheet">
 </head>
 
 <body id="page-top">
-
+<!-- navigation-->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
     <div class="container">
       <a class="navbar-brand js-scroll-trigger" href="#page-top"><img class="logo" src="img/logo.jpg" alt="logo">Muzik Kontrol</a>
@@ -30,11 +30,24 @@
           <li class="nav-item">
             <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#article">Article</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#news">News</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#decouverte">Decouverte</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#partenaires">Partenaires</a>
+          </li>
         </ul>
       </div>
     </div>
   </nav>
-
+    
+  <!-- image principale-->
      <div class="jumbotron big-banner" style="height: 500px; padding-top: 150px;">
 
   <header class="text-white">
@@ -55,7 +68,7 @@
       </div>
     </div>
   </section>
-
+<!-- services (pb) signifie padding bottom raccourci bs-->
   <section id="services" class="bg-light text-center" >
 <div class="container">
   <div class="col-lg-12 mx-auto text-center pb-5">
@@ -110,6 +123,89 @@
         </div>
       </div>
     </div>
+</div>
+  </section>
+
+  <section id="contact">
+    <div class="container">
+    <div class="col-lg-8 mx-auto info">
+          <h2>Contact</h2>
+              <a href="https://goo.gl/maps/qUKwzLYqT5oHEsaL7"><p><i class="fa fa-map-marker fa-2x"></i>92 Rue St Dizier Nancy, France</p></a>
+              <p><i class="fa fa-phone fa-2x"></i>+33 3 54 16 24 51</p></a>
+              <a href="https://www.instagram.com/musickontrol"><p><i class="fa fa-instagram fa-2x"></i>instagram</p></a>
+              <a href="https://fr-fr.facebook.com/pages/category/Electronics-Store/Music-Kontrol-170296356950248/"><p><i class="fa fa-facebook fa-2x"></i>facebook</p></a>
+              <a href="musickontrol@sfr.fr"><p><i class="fa fa-envelope-o fa-2x"></i>musickontrol@sfr.fr</p></a>
+        </div>
+      </div>
+    </div>
+  </section>
+   
+   <section id="article" class="bg-light text-center" >
+<div class="container">
+  <div class="col-lg-12 mx-auto text-center pb-5">
+      <h2>Article</h2>
+  </div>
+  <div class="row">
+    <!--form -->
+    <div class="col-lg-7 mx-auto">
+      <div class="bg-white rounded-lg shadow-sm p-5" id = "formArtic">
+        <!--  form tabs -->
+        <ul role="tablist" class="nav bg-light nav-pills rounded-pill nav-fill mb-3">
+          <li class="nav-item">
+            <a data-toggle="pill" href="#nav-tab-card" class="nav-link active rounded-pill">
+                              <i class="fab fa-wpforms"></i>
+                                Formulaire Article
+                            </a>
+          </li>
+        </ul>
+        <!-- End -->
+        <!--  form content -->
+        <div class="tab-content">
+
+          <!--  info-->
+          <div id="nav-tab-card" class="tab-pane fade show active">
+            <form role="form">
+            <div class="form-group">
+                <label for="username">Titre</label>
+                <input type="text" name="titre" placeholder="Tire" required class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="username">Auteur</label>
+                <input type="text" name="auteur" placeholder="Jason Doe" required class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="cardNumber">Contenu</label>
+                <div class="input-group">
+                <textarea id="form7" class="md-textarea form-control" rows="3" class="form-control" required></textarea>  
+                </div>
+              </div>
+              <button type="submit" class="subscribe btn btn-primary btn-block rounded-pill shadow-sm"> Confirm  </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+        <img src="img/price.jpg" class="card-img-top" alt="pricing">
+        <div class="card-body text-center">
+          <h5 class="card-title mb-0">Quelle carte de son choisir ?</h5>
+          <div class="card-text text-black-50">Pour débutant , intermediare ou proffesionel. Guide comment choisir une carte son par à port à certain critere</div>
+          <a href="#" class="btn btn-primary btnPlus crud">Voir plus</a>
+          <button type="button" class="btn btn-danger btndel crud" >supprimer</button>
+          <button type="button" class="btn btn-warning crud">modifier</button>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
+    </div>
+  </div>
+      </div>
+    </div>
+    <button type="button" class="btn btn-outline-success" id="createArtic">Créer un nouvelle article</button>
 </div>
   </section>
 
