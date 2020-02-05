0fa72814e38b25c6f46c307b9621ad58d3d4c3e8
kanoe <noe.ngongo20@gmail.com>
2020-02-05 16:32:20 +0100

    class type_article & user

---
+++ model/type_article.php
@@ -0,0 +1,18 @@
+<?php
+class Type_Article
+{
+    private $_id;
+    private $nom;
+    
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+}
---
+++ model/user.php
@@ -0,0 +1,40 @@
+<?php
+class User
+{
+    private $_id;
+    private $_prenom;
+    private $_nom;
+    private $_age;
+    private $_mdp;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getPrenom()
+    {
+        return $this->_prenom;
+    }
+
+    public function ger()
+    {
+        return $this->id;
+    }
+
+    public function save()
+    {
+        $q = $this->_db->prepare('INSERT INTO personnages(nom, forcePerso, degats, niveau, experience) VALUES(:nom, :forcePerso, :degats, :niveau, :experience)');
+    }
+
+    public function update($article ,$titre,$_contenu) {
+        $this->_db->exec('UPDATE $article SET titre= $titre ,contenu='".$resultat."' WHERE id=".$id');
+    }
+
+    public function delete($id) {
+        $this->_db->exec('DELETE * FROM Article');
+
+    }
+
+
+}
